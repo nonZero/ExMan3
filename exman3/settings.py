f158bf2d027836382809dc8f173e0d5156e15d46
Udi Oron <udioron@gmail.com>
2016-07-21 15:45:59 +0300

    Added django extensions

--- exman3/settings.py
+++ exman3/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # builtin
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd-party
+    'django_extensions',
+
 ]
 
 MIDDLEWARE_CLASSES = [
