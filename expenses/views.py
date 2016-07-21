312e53b971bfb89f189471c82843778fc3c11ce7
Udi Oron <udioron@gmail.com>
2016-07-21 17:51:03 +0300

    cleanup

--- expenses/views.py
+++ expenses/views.py
@@ -8,22 +8,9 @@ from . import models
 # single: modelname_detail
 
 def expense_list(request):
-    # qs = models.Expense.objects.all()
-    qs = models.Expense.objects.order_by('-amount')[:10]
-    # s = "<br/>".join([o.title for o in qs])
-    # return HttpResponse(s)
-
-    desc = """Lorem ipsum dolor sit amet,
-consectetur adipisicing elit.
-Accusantium dolorem doloremque
-fugiat provident recusandae sunt!"""
+    qs = models.Expense.objects.order_by('-created_at')
 
     # TODO: fix name to meet convention
     return render(request, "expense_list.html", {
         'object_list': qs,
-        'foo': 123,
-        'bar': "kukukukukukuk<b>ukukuku</b>kukkukukukku",
-        'x': random.randint(1, 10),
-        'desc': desc,
-        'colors': ['red', 'green', 'blue'],
     })
